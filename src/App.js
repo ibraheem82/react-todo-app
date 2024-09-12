15774b9e91398e5f28c6b29bf54dd63c1f4fe558
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-12 01:36:27 +0100

    Integrate ToDoMenu component into App
    - Import `ToDoMenu` component from the components directory.
    - Add `<ToDoMenu />` to the `App` component to display the to-do menu within the application.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import ToDoMenu from './components/ToDoMenu';
 
 function App() {
   return (
     <div className="App bg-red-500 h-screen bg-gradient-to-r from-purple-400 via-pink-200 to-pink-300 text-white">
       <h1 className="text-violet-900 mb-8 text-5xl pt-12 font-bold">Todo List App 📃</h1>
+
+      <ToDoMenu />
     </div>
   );
 }

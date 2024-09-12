14cde5d092ef152f8039caa0833fc2a7ae325970
ibraheem82 <ibraheemomikunle82@gmail.com>
2024-09-12 01:35:27 +0100

    Enhance ToDoMenu component with input field and add button
    - Import `RiAddLargeFill` icon from `react-icons` for use as the add button icon.
    - Add an input field with a gradient background and styled placeholder for entering tasks.
    - Style the input field and button with Tailwind CSS classes for a responsive and visually appealing design.
    - Include the add button with an icon for adding tasks to the to-do list.

--- src/components/ToDoMenu.jsx
+++ src/components/ToDoMenu.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { RiAddLargeFill } from "react-icons/ri";
 const ToDoMenu = () => {
     return (
-        <div>
-            <h1>To Do Menu</h1>
+        <div className='mx-auto mt-8'>
+            <div className='flex items-center justify-center mt-4'>
+                <input type="text"  className='w-[350px] border-2 border-blue-900 bg-gradient-to-r from-sky-900 via sky-500 to-indigo-200 font-bold rounded-md px-3 py-2 mr-3 placeholder:text-white' placeholder='Enter Task...'/>
+                <button>
+                <RiAddLargeFill />
+                </button>
+            </div>
         </div>
     );
 }
